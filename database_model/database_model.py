016a022fcf9a7e478eec571016b56a68311ec2bf
Sudipta <powerboy4321@outlook.com>
2025-11-07 00:20:01 +0530

    model added for indexing , notification log and webhook logs schema

--- database_model/database_model.py
+++ database_model/database_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String , Text , Integer, DateTime, Boolean, ForeignKey , DECIMAL as Decimal
+from sqlalchemy import Column, String , Text , JSON,Integer, DateTime, Boolean, ForeignKey , DECIMAL as Decimal
 from sqlalchemy.dialects.postgresql import UUID , ARRAY as Array
 import uuid
 from sqlalchemy.orm import relationship
@@ -16,7 +16,7 @@ class file_info(Base):
     file_id= Column(UUID, primary_key=True , nullable=False,default=uuid.uuid4)
     filelog = relationship("activity_log", back_populates="fileid")
     filemd = relationship("file_metadata", back_populates="filemetadata")
-
+    fileaccessindex = relationship("embedded_data", back_populates="fileindexid")
     owner_id = Column(UUID,ForeignKey("user_data.user_id"),nullable=False)
     
     fileinfo = relationship("user", back_populates="usernfo")
@@ -47,6 +47,7 @@ class user(Base):
     usernfo = relationship("file_info", back_populates="fileinfo")
     userlogdetail = relationship("activity_log", back_populates="userlog")
     useraccess = relationship("file_access", back_populates="shareduser")
+    usernotifications = relationship("notification_settings", back_populates="usernotify")
 
 
 #Define the organization model
@@ -59,6 +60,7 @@ class organization(Base):
     created_at=Column(DateTime, nullable=False, default='now()')
     storage_limit=Column(String, nullable=False) #10GB default limit
     orginfo = relationship("subscription", back_populates="orgidinfo")
+    webhook = relationship("webhook_logs", back_populates="webhoookid")
 
 
 #define the plan model
@@ -123,7 +125,7 @@ class file_analytics(Base):
 
 #define file access model
 class file_access(Base):
-    __tablename__ = 'fileAcces'
+    __tablename__ = 'fileacces'
     acces_id=Column(UUID, primary_key=True , nullable=False,default=uuid.uuid4)
     file_id=Column(UUID, ForeignKey("file_data.file_id"), nullable=False)
     fileaccessid =relationship("file_info" , back_populates="fileaccess")
@@ -134,4 +136,37 @@ class file_access(Base):
     max_download=Column(Integer)
     pwd_protect=Column(Boolean, nullable=False)
     access_status=Column(Text, nullable=False, default='Active')
+
+#define model for embedded data indexing
+
+class embedded_data(Base):
+    __tablename__ = 'embedd_index'
+    embed_id=Column(UUID, primary_key=True , nullable=False,default=uuid.uuid4)
+    file_id=Column(UUID, ForeignKey("file_data.file_id"), nullable=False)
+    fileindexid =relationship("file_info", back_populates="fileaccessindex")
+    vector_data=Column(JSON, nullable=False)
+    model_used=Column(Text, nullable=False)
+    created_at=Column(DateTime, nullable=False)
+
+#define model for notification settings
+class notification_settings(Base):
+    __tablename__ = 'notification'
+    notification_id =Column(UUID, primary_key=True , nullable=False,default=uuid.uuid4)
+    User_id=Column(UUID, ForeignKey("user_data.user_id"), nullable=False)
+    usernotify = relationship("user", back_populates="usernotifications")
+    notification_type=Column(Text, nullable=False)
+    Notification_message=Column(Text, nullable=False)
+    is_read=Column(Boolean, nullable=False, default=False)
+    create_at =Column(DateTime,nullable=False)
+
+#define modelfor webhook logs
+class webhook_logs(Base):
+    __tablename__ = 'webhookstable'
+    webhook_id=Column(UUID, primary_key=True , nullable=False,default=uuid.uuid4)
+    org_id=Column(UUID,ForeignKey("org.org_id"), nullable=False)
+    webhoookid = relationship("organization", back_populates="webhook")
+    url=Column(Text, nullable=False)
+    event_type=Column(Text, nullable=False)
+    secret_token=Column(Text, nullable=False)
+    is_active=Column(DateTime, nullable=False)
 
